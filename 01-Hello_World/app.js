70939ef626ad1caa8167dec297a9c1207eb70e92
Jorge García Gómez <jorge@jorgegarciagomez.es>
2020-02-06 21:52:00 +0100

    Ciclo de eventos node

---
+++ 01-Hello_World/app.js
@@ -0,0 +1,4 @@
+let nombre = "Jorge";
+
+console.log("Hola Mundo");
+
---
+++ 01-Hello_World/app2.js
@@ -0,0 +1,10 @@
+function saludar(nombre) {
+    let mensaje = `Hola ${ nombre }`;
+    return mensaje;
+}
+
+let saludo = saludar("Jorge");
+
+
+console.log(saludo);
+
