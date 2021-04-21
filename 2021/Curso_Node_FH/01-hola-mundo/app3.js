01f1ab79d0a885ac23f79249faeddc3f3c3a4b76
Jorge <jorge@jorgegarciagomez.es>
2021-04-21 21:45:20 +0200

    Update app3.js

--- 2021/Curso_Node_FH/01-hola-mundo/app3.js
+++ 2021/Curso_Node_FH/01-hola-mundo/app3.js
@@ -1,2 +1,21 @@
 
 
+console.log("Inicio del programa");
+
+setTimeout(() => {
+    console.log("Hola");
+}, 3000);
+
+
+
+setTimeout(() => {
+    console.log("2");
+}, 0);
+
+
+setTimeout(() => {
+    console.log("3");
+}, 0);
+
+
+console.log("Fin del programa");
