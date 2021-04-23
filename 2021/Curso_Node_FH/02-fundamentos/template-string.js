ae2dcee57335d5333543b851ff757a7aeadd41df
Jorge <jorge@jorgegarciagomez.es>
2021-04-23 06:06:25 +0200

    template string

---
+++ 2021/Curso_Node_FH/02-fundamentos/template-string.js
@@ -0,0 +1,18 @@
+const nombre = "Jorge";
+const apellidos = "García";
+
+const normal = nombre + ' ' + apellidos;
+const template = `${nombre} ${apellidos}`;
+
+console.log(normal);
+console.log(template);
+
+const html = `
+
+<h1>Hola</h1>
+<p>Mundo</p>
+
+
+`;
+
+console.log(html);
