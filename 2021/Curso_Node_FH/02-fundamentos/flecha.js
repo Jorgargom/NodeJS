3dac2574be13854a2c748169d175013859da2f91
Jorge <jorge@jorgegarciagomez.es>
2021-04-23 20:06:40 +0200

    flecha

---
+++ 2021/Curso_Node_FH/02-fundamentos/flecha.js
@@ -0,0 +1,14 @@
+// function sumar( a, b ) {
+//     return a + b;
+// }
+
+const sumar = (a, b) => a + b;
+
+const saludar = () => 'Hola Mundo';
+
+
+console.log(  sumar( 5, 10 )   );
+console.log(  saludar()   );
+
+
+
