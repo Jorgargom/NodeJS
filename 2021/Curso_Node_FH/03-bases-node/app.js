528b47e2f7d3b26a891126edc8729031e13e7731
Jorge <jorge@jorgegarciagomez.es>
2021-04-25 08:40:24 +0200

    Tabla del 5

---
+++ 2021/Curso_Node_FH/03-bases-node/app.js
@@ -0,0 +1,17 @@
+
+console.clear();
+console.log('=================');
+console.log('== Tabla del 5 ==');
+console.log('=================');
+
+const base = 5;
+
+for (let index = 1; index <= 10; index++) {
+    console.log(`${base} x ${index} = ${base * index}`);
+}
+
+
+console.log('=================');
+console.log('=================');
+console.log('=================');
+
