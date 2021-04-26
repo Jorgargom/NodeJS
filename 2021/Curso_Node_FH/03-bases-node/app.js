501bdf52b146b63b32385442f5878e7a05201b8c
Jorge <jorge@jorgegarciagomez.es>
2021-04-26 19:50:52 +0200

    require fs

--- 2021/Curso_Node_FH/03-bases-node/app.js
+++ 2021/Curso_Node_FH/03-bases-node/app.js
@@ -1,17 +1,29 @@
 
+
+const fs = require('fs');
+
+
 console.clear();
-console.log('=================');
-console.log('== Tabla del 5 ==');
-console.log('=================');
+
 
 const base = 5;
+let salida = '';
+
+console.log('=================');
+console.log(`== Tabla del ${base} ==`);
+console.log('=================');
 
 for (let index = 1; index <= 10; index++) {
-    console.log(`${base} x ${index} = ${base * index}`);
+    salida += `${base} x ${index} = ${base * index}\n`;
 }
 
 
-console.log('=================');
-console.log('=================');
-console.log('=================');
+console.log(salida);
+
+
+fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+    if (err) throw err;
+
+    console.log(`tabla-${base}.txt creado`);
+})
 
