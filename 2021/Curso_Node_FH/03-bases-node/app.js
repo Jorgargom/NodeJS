9d95a8bbce66ac34c692d6472e41f8e04e3cb992
Jorge <jorge@jorgegarciagomez.es>
2021-04-28 20:45:23 +0200

    recibir informacion desde la consola

--- 2021/Curso_Node_FH/03-bases-node/app.js
+++ 2021/Curso_Node_FH/03-bases-node/app.js
@@ -5,8 +5,18 @@ const { crearArchivo } = require('./helpers/multiplicar');
 console.clear();
 
 
-const base = 5;
+const [ , , arg3 = 'base=5'] = process.argv;
+const [, base = 5 ] = arg3.split('=');
+
+console.log(arg3);
+ 
+
+
+
+
+/* const base = 5; */
 
 crearArchivo(base)
     .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
     .catch( err => console.log(err) );
+ 
