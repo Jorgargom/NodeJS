2f13e7df2e1fd78ee130fd42ad1cd8a39f09d120
Jorge <jorge@jorgegarciagomez.es>
2021-05-01 20:02:57 +0200

    yargs

--- 2021/Curso_Node_FH/03-bases-node/app.js
+++ 2021/Curso_Node_FH/03-bases-node/app.js
@@ -1,14 +1,15 @@
 
 
 const { crearArchivo } = require('./helpers/multiplicar');
+const argv =  require('yargs').argv;
+
 
 console.clear();
 
 
-const [ , , arg3 = 'base=5'] = process.argv;
-const [, base = 5 ] = arg3.split('=');
-
-console.log(arg3);
+console.log(process.argv);
+console.log(argv);
+console.log('base: yargs', argv.base);
  
 
 
@@ -16,7 +17,7 @@ console.log(arg3);
 
 /* const base = 5; */
 
-crearArchivo(base)
+/* crearArchivo(base)
     .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
-    .catch( err => console.log(err) );
+    .catch( err => console.log(err) ); */
  
