57ab6fe0bc4ea0e5249e92abaffe5a1fcdc6cbda
Jorge <jorge@jorgegarciagomez.es>
2021-05-02 10:45:09 +0200

    config yargs

--- 2021/Curso_Node_FH/03-bases-node/app.js
+++ 2021/Curso_Node_FH/03-bases-node/app.js
@@ -1,29 +1,9 @@
 
 const { crearArchivo } = require('./helpers/multiplicar');
-const argv =  require('yargs')
-                .option('b', {
-                    alias: 'base',
-                    type: 'number',
-                    demandOption: true
-                })
-                .option('l', {
-                    alias: 'listar',
-                    type: 'boolean',
-                    demandOption: true,
-                    default: false
-                })
-                .check((argv, options) => {
-                    if (isNaN(argv.b)) {
-                        throw 'La base tiene que ser un número'
-                    }
-                    return true;
-                })
-                .argv;
+const argv = require('./config/yargs');
 
 
 console.clear();
-
-console.log(argv);
 
 crearArchivo(argv.b, argv.l)
     .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
---
+++ 2021/Curso_Node_FH/03-bases-node/config/yargs.js
@@ -0,0 +1,24 @@
+const argv =  require('yargs')
+                .option('b', {
+                    alias: 'base',
+                    type: 'number',
+                    demandOption: true,
+                    describe: 'Es la base de la tabla de multiplicar'
+
+                })
+                .option('l', {
+                    alias: 'listar',
+                    type: 'boolean',
+                    default: false,
+                    describe: 'Muestra la tabla de multiplicar en consola'
+                })
+                .check((argv, options) => {
+                    if (isNaN(argv.b)) {
+                        throw 'La base tiene que ser un número'
+                    }
+                    return true;
+                })
+                .argv;
+
+
+                module.exports = argv;
