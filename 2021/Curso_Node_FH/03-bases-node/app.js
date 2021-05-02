9aa2c58909729dae568b7f0a805e7ee5960cca75
Jorge <jorge@jorgegarciagomez.es>
2021-05-02 10:37:58 +0200

    Configuration options yargs

--- 2021/Curso_Node_FH/03-bases-node/app.js
+++ 2021/Curso_Node_FH/03-bases-node/app.js
@@ -1,23 +1,30 @@
 
-
 const { crearArchivo } = require('./helpers/multiplicar');
-const argv =  require('yargs').argv;
+const argv =  require('yargs')
+                .option('b', {
+                    alias: 'base',
+                    type: 'number',
+                    demandOption: true
+                })
+                .option('l', {
+                    alias: 'listar',
+                    type: 'boolean',
+                    demandOption: true,
+                    default: false
+                })
+                .check((argv, options) => {
+                    if (isNaN(argv.b)) {
+                        throw 'La base tiene que ser un número'
+                    }
+                    return true;
+                })
+                .argv;
 
 
 console.clear();
 
-
-console.log(process.argv);
 console.log(argv);
-console.log('base: yargs', argv.base);
- 
 
-
-
-
-/* const base = 5; */
-
-/* crearArchivo(base)
+crearArchivo(argv.b, argv.l)
     .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
-    .catch( err => console.log(err) ); */
- 
+    .catch( err => console.log(err) ); 
--- 2021/Curso_Node_FH/03-bases-node/helpers/multiplicar.js
+++ 2021/Curso_Node_FH/03-bases-node/helpers/multiplicar.js
@@ -1,22 +1,23 @@
 
 const fs = require('fs');
 
-const crearArchivo = async(base = 5) =>{
+const crearArchivo = async(base = 5, listar = false) =>{
 
     try {
-        console.log('=================');
-        console.log(`== Tabla del ${base} ==`);
-        console.log('=================');
-    
+
         let salida = '';
         
         for (let index = 1; index <= 10; index++) {
             salida += `${base} x ${index} = ${base * index}\n`;
         }
         
-        
-        console.log(salida);
-        
+        if (listar) {
+            console.log('=================');
+            console.log(`== Tabla del ${base} ==`);
+            console.log('=================');
+            console.log(salida);
+        } 
+            
         fs.writeFileSync(`tabla-${base}.txt`, salida);
         
         console.log(`tabla-${base}.txt creado`);
